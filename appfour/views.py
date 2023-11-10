cf8b1baa3bade344a94df7002c7b203fd087c967
Hariharasudhan5 <harihaharv5555@gmail.com>
2023-11-10 16:40:22 +0530

    first commit

---
+++ appfour/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+def hari(request):
+    return render(request,'hari.html')
+
+def helo(request):
+    return render(request,'helo.html')
+
+def hii(request):
+    return render(request,'ganesh.html')
+
+
+def bruhhh(request):
+    return render(request,'uday.html')
